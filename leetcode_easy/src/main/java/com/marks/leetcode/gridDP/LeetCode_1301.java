e8b524f08e37c6c347e9882096de6930538011bf
2741140081 <2741140081@qq.com>
2024-07-22 18:53:55 +0800

    add LeetCode_1301.java

---
+++ leetcode_easy/src/main/java/com/marks/leetcode/gridDP/LeetCode_1301.java
@@ -0,0 +1,70 @@
+package com.marks.leetcode.gridDP;
+
+import java.util.List;
+
+public class LeetCode_1301 {
+    /**
+     * 给你一个正方形字符数组 board ，你从数组最右下方的字符 'S' 出发。
+     * 你的目标是到达数组最左上角的字符 'E' ，数组剩余的部分为数字字符 1, 2, ..., 9 或者障碍 'X'。
+     * 在每一步移动中，你可以向上、向左或者左上方移动，可以移动的前提是到达的格子没有障碍。
+     * 一条路径的 「得分」 定义为：路径上所有数字的和。
+     * 请你返回一个列表，包含两个整数：第一个整数是 「得分」 的最大值，第二个整数是得到最大得分的方案数，请把结果对 10^9 + 7 取余。
+     * 如果没有任何路径可以到达终点，请返回 [0, 0] 。
+     *
+     * tips:
+     * 2 <= board.length == board[i].length <= 100
+     * @param board
+     * @return
+     */
+    public int[] pathsWithMaxScore(List<String> board) {
+        int[] result;
+        result = method_01(board);
+        return result;
+    }
+
+    /**
+     * E1:
+     * 输入：board = ["E23","2X2","12S"]
+     * 输出：[7,1]
+     *
+     * E2:
+     * 输入：board = ["E12","1X1","21S"]
+     * 输出：[4,2]
+     *
+     * E3:
+     * 输入：board = ["E11","XXX","11S"]
+     * 输出：[0,0]
+     *
+     * 思路:
+     * dp[][]
+     * 1. 判断是否可达
+     * 2. 求得可达到的最大值
+     * 3. 最大值的前提下, 获取方案数
+     *
+     * 先做1,2
+     * 前提条件: board[i][j] != 'X'
+     * dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-1], dp[i][j-1])
+     * 可以初始化dp[m-1][i] 和 dp[i][n-1]
+     *
+     * 初始化
+     * dp[m-1][n-1] = 0;
+     *
+     * if board[m-1][i] != 'X'
+     * dp[m-1][i] = dp[m-1][i-1] + board[m-1][i]
+     * else dp[m-1][i] = 0;
+     *
+     * dp[i][n-1] = dp[i-1][n-1] + board[i][n-1]
+     *
+     * 对于3, 需要多一个二维数组存放方案数
+     * dpNum[m-1][n-1] = 1
+     * 右边界和下方边界的值初始化都是1
+     *
+     * @param board
+     * @return
+     */
+    private int[] method_01(List<String> board) {
+        final int MOD = 1000000007;
+
+        return new int[2];
+    }
+}
