8b9ba467d358aad8122347a550304b4d97595c43
2741140081 <2741140081@qq.com>
2024-08-14 16:19:49 +0800

    Complete LeetCode_300.java

---
+++ leetcode_easy/src/test/java/com/marks/leetcode/classic_linear_DP/LIS/LeetCode_300Test.java
@@ -0,0 +1,26 @@
+package com.marks.leetcode.classic_linear_DP.LIS;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <p>项目名称:  </p>
+ * <p>文件名称:  </p>
+ * <p>描述: [类型描述] </p>
+ *
+ * @author marks
+ * @version v1.0
+ * @data 2024/8/14 15:18
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+class LeetCode_300Test {
+
+    @Test
+    void lengthOfLIS() {
+        // nums = [10,9,2,5,3,7,101,18]
+        // nums = [1,3,6,7,9,4,10,5,6]
+        int[] nums = new int[]{1,3,6,7,9,4,10,5,6};
+        System.out.println(new LeetCode_300().lengthOfLIS(nums));
+    }
+}
