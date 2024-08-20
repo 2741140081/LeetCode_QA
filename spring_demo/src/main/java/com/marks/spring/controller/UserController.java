8bb5c6a97ae2dbce3dc1538439e28df381136396
2741140081 <2741140081@qq.com>
2024-08-20 16:38:10 +0800

    Add Spring IOC 注解和druid配置

---
+++ spring_demo/src/main/java/com/marks/spring/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.marks.spring.controller;
+
+/**
+ * <p>项目名称:  </p>
+ * <p>文件名称:  </p>
+ * <p>描述: [类型描述] </p>
+ *
+ * @author marks
+ * @version v1.0
+ * @data 2024/8/20 15:42
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+public class UserController {
+}
