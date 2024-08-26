d3295be1e2a77cd6db596401a8d44daee3a6eaa7
2741140081 <2741140081@qq.com>
2024-08-26 10:43:58 +0800

    Add LeetCode_121.Java dp解决, 内含多种方法

--- leetcode_easy/src/main/java/com/marks/leetcode/state_machine_DP/LeetCode_121.java
+++ leetcode_easy/src/main/java/com/marks/leetcode/state_machine_DP/LeetCode_121.java
@@ -36,13 +36,102 @@ public class LeetCode_121 {
         int result = 0;
 //        result = method_01(prices);
 //        result = method_02(prices);
-        result = method_03(prices);
+//        result = method_03(prices);
+//        result = method_04(prices);
+//        result = method_05(prices);
+        result = method_06(prices);
         return result;
+    }
+
+    /**
+     * @Description: [动态规划: 空间优化]
+     * @param prices
+     * @return int
+     * @author marks
+     * @CreateDate: 2024/8/26 10:40
+     * @update: [序号][YYYY-MM-DD] [更改人姓名][变更描述]
+     */
+    private int method_06(int[] prices) {
+        int n = prices.length;
+        if (n <= 1) {
+            return 0;
+        }
+        int[] dp = new int[2];
+        dp[0] = 0;
+        dp[1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[0] = Math.max(dp[0], dp[1] + prices[i]);
+            dp[1] = Math.max(dp[1], -prices[i]);
+        }
+        return dp[0];
+    }
+
+    /**
+     * @Description: [动态规划:滚动数组]
+     * @param prices
+     * @return int
+     * @author marks
+     * @CreateDate: 2024/8/26 10:30
+     * @update: [序号][YYYY-MM-DD] [更改人姓名][变更描述]
+     */
+    private int method_05(int[] prices) {
+        int n = prices.length;
+        if (n <= 1) {
+            return 0;
+        }
+        int[][] dp = new int[2][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        int curr = 0;
+        for (int i = 1; i < n; i++) {
+            curr = i % 2;
+            int pre = 1 - curr;
+            dp[curr][0] = Math.max(dp[pre][0], dp[pre][1] + prices[i]);
+            dp[curr][1] = Math.max(dp[pre][1], -prices[i]);
+        }
+        return dp[curr][0];
+    }
+
+    /**
+     * @Description: [动态规划
+     * 1.你不能买入股票前卖出股票
+     * 2.最多只允许一次交易
+     *
+     * 因此 当天是否持股 是一个很重要的因素，而当前是否持股和昨天是否持股有关系，为此我们需要把 是否持股 设计到状态数组中。
+     * 转态定义:
+     * dp[i][j]：下标为 i 这一天结束的时候，手上持股状态为 j 时，我们持有的现金数。
+     * 换种说法：dp[i][j] 表示天数 [0, i] 区间里，下标 i 这一天状态为 j 的时候能够获得的最大利润。其中：
+     * a. j = 0，表示当前不持股；
+     * b. j = 1，表示当前持股。
+     * ]
+     * @param prices
+     * @return int
+     * @author marks
+     * @CreateDate: 2024/8/26 10:12
+     * @update: [序号][YYYY-MM-DD] [更改人姓名][变更描述]
+     */
+    private int method_04(int[] prices) {
+        int n = prices.length;
+        if (n <= 1) {
+            return 0;
+        }
+        int[][] dp = new int[n][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+        }
+        return dp[n - 1][0];
     }
 
     /**
      * @Description: [
      * 使用单调栈来解决
+     *
+     * 只是单纯使用单调栈来解决这个问题，并非是单调栈是最优解
+     * 时间复杂度是O(n^2)
+     * 空间复杂度O(n)
      * ]
      * @param prices
      * @return int
@@ -58,8 +147,15 @@ public class LeetCode_121 {
         // 单调递增栈
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int ans = 0;
+        /*
+        1. 如果栈为空, 则直接入栈
+        2. 如果元素值大于栈顶元素值, 则直接入栈
+        3. 如果元素值小于栈顶元素值, 则遍历栈, 弹出栈顶元素, 直到栈顶元素小于入栈元素值
+        4. 使用ans = Math.max(ans, stack.pop() - stack.peekLast); 即弹出栈顶元素 - 栈底元素
+         */
         for (int i = 0; i <= n; i++) {
-            int curValue = i == n ? 0 : prices[i];
+            // -1 的作用是作为一个哨兵，使得循环结束时栈内元素只剩余一个-1, 让其他元素全部出栈, 因为price[i] >= 0的
+            int curValue = i == n ? -1 : prices[i];
             while (!stack.isEmpty() && stack.peek() >= curValue) {
                 // push: 入栈, pop: 出栈, peek:获取栈顶元素, isEmpty: 判断栈是否为空
                 int top = stack.pop();
