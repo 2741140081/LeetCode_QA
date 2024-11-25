a4b9a934596135fb324d87d348fb2f8e04045e23
2741140081 <2741140081@qq.com>
2024-11-25 11:29:39 +0800

    Add LeetCode_2226.java

---
+++ leetcode_easy/src/test/java/com/marks/leetcode/binary_algorithm/LeetCode_275Test.java
@@ -0,0 +1,27 @@
+package com.marks.leetcode.binary_algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <p>项目名称:  </p>
+ * <p>文件名称:  </p>
+ * <p>描述: [类型描述] </p>
+ *
+ * @author marks
+ * @version v1.0
+ * @date 2024/11/25 10:18
+ * @update [序号][日期YYYY-MM-DD] [更改人姓名][变更描述]
+ */
+class LeetCode_275Test {
+
+    @Test
+    void hIndex() {
+        // citations = [0,1,3,5,6]
+        // [1,2,100]
+        int[] citations = {0,1,3,5,6};
+        int result = new LeetCode_275().hIndex(citations);
+        System.out.println(result);
+    }
+}
