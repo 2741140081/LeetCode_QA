e3ca4d89611b0ebf4d2704aa09c392cc38a6ba25
2741140081 <2741140081@qq.com>
2025-05-16 15:17:29 +0800

    Add new demo

---
+++ redemo/src/main/java/com/marks/redemo/RedemoApplication.java
@@ -0,0 +1,13 @@
+package com.marks.redemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedemoApplication.class, args);
+	}
+
+}
